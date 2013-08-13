b0e4356c31910b59fdfcb277996f691f82c4b951
jnewell88 <jnewell88@live.com>
2013-08-13 08:33:20 -0500

    Update problem3.c
    Added more comments

--- problem3.c
+++ problem3.c
@@ -6,27 +6,29 @@
 #include <stdio.h>
 
 int main(){
+    //declare and initialize variables
     long long i, sqi;
     long long value, large = 600851475143LL;
     long long max = 0LL;
-
     i   = 2LL;
     sqi = 4LL; //i*i
-    for(value = large; sqi <= value ; sqi +=  2LL * i++ + 1LL){
-        while(value % i == 0LL){
-            value /= (max=i);
+    
+    
+    for(value = large; sqi <= value ; sqi +=  2LL * i++ + 1LL){//while sqi is less than or equal to value
+        while(value % i == 0LL){ //while value is divisible by i
+            value /= (max=i); //divide value by max=i
         }
     }
 
-    if(value != 1LL && value != large){
-        max = value;
+    if(value != 1LL && value != large){//if value is not equal to 1 or the value for large
+        max = value; //set maximum as the value of value
     }
-    if(max == 0LL){
-        max = large;
+    if(max == 0LL){ //if max is equal to 0
+        max = large;    //set max as the value of large
     }
     
     printf("-----------------------Problem 3----------------------------\n");
-    printf("The largest prime factor of the number %lld is %lld\n", large, max);
+    printf("The largest prime factor of the number %lld is %lld\n", large, max); //output result
     printf("------------------------------------------------------------\n");
     return 0; 
 }
